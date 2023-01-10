5dd5b616468c335cd236c3a73a4f174a2f9f06e4
Prabhat <prabhatsingh55rocks@gmail.com>
2023-01-10 18:43:23 +0530

    created user and application module

---
+++ src/model/AppModule.ts
@@ -0,0 +1,43 @@
+import { User } from "./User";
+
+export class AppModule {
+  users: User[];
+  graph: number[][];
+  hasOutput: boolean;
+  output: number[][];
+//   alert: {
+//     hasAlert: boolean;
+//     variant: string;
+//     description: string;
+//   };
+  searchedUsers: string[];
+  onNewUserEntry: (user: User) => void;
+  onNewRelationshipEntry: (user1: string, user2: string) => void;
+  onSearch: (user1: string, user2: string) => void;
+
+  constructor(
+    users: User[],
+    graph: number[][],
+    hasOutput: boolean,
+    output: number[][],
+    // alert: {
+    //   hasAlert: boolean;
+    //   variant: string;
+    //   description: string;
+    // },
+    searchedUsers: string[],
+    onNewUserEntry: (user: User) => void,
+    onNewRelationshipEntry: (user1: string, user2: string) => void,
+    onSearch: (user1: string, user2: string) => void
+  ) {
+    this.users = users;
+    this.graph = graph;
+    this.hasOutput = hasOutput;
+    this.output = output;
+    // this.alert = alert;
+    this.searchedUsers = searchedUsers;
+    this.onNewUserEntry = onNewUserEntry;
+    this.onNewRelationshipEntry = onNewRelationshipEntry;
+    this.onSearch = onSearch;
+  }
+}
