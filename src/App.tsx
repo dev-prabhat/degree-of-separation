4acd26a715d9ca131ee9e66ff4de535c046ac4df
Prabhat <prabhatsingh55rocks@gmail.com>
2023-01-10 18:40:27 +0530

    remove boiler plate code from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { AddPeople } from "./components/AddPeople";
+import { Home } from "./components/Home";
+import { SetRelationShip } from "./components/SetRelationship";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home />
+      <AddPeople />
+      <SetRelationShip />
     </div>
   );
 }
